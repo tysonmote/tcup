ff5526be55b1177272915c4cb8bb0b9ac54bd639
Tyson Tate <tyson@tysontate.com>
2013-04-24 00:25:34 -0700

    Initial version.

---
+++ README.md
@@ -0,0 +1,34 @@
+tcup
+====
+
+tcup is an HTTPS server that forwards data to a UDP address. tcup can be
+configured to expect a `X-Token` header for simple authentication.
+
+    Usage: ./tcup [flags]
+      -help=false: Print usage info
+      -cert="cert.pem": Path to SSL certificate file
+      -key="key.pem": Path to SSL key file
+      -in="127.0.0.1:1984": TCP listening address
+      -out="127.0.0.1:1984": UDP destination address
+      -token="": Expected "X-Token" header for all requests
+
+tcup returns the following status codes:
+
+* 200: Request successfully forwarded.
+* 400: Empty request body.
+* 401: Incorrect X-Token header given.
+* 500: There was an error reading the request body or sending it to the
+  destination UDP address. Check the response body for an error message.
+
+How do I install it?
+--------------------
+
+Install [Go](http://golang.org) and then run:
+
+    go install github.com/tysontate/tcup
+
+Should I use it?
+----------------
+
+No.
+
---
+++ main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+var (
+	// Command-line flags
+	out   = flag.String("out", "127.0.0.1:1984", "UDP destination address")
+	in    = flag.String("in", "127.0.0.1:1984", "TCP listening address")
+	token = flag.String("token", "", "Expected \"X-Token\" header for all requests")
+	cert  = flag.String("cert", "cert.pem", "Path to SSL certificate file")
+	key   = flag.String("key", "key.pem", "Path to SSL key file")
+	help  = flag.Bool("help", false, "Print usage info")
+
+	// Stdout logging
+	logger *log.Logger
+
+	// Statsd connection
+	writeLock sync.Mutex
+	destConn  net.Conn
+
+	// Stats
+	requestsReceived int
+	bytesReceived    int
+)
+
+// Print usage information and exit.
+func usage() {
+	fmt.Printf("Usage: %s [flags]\n", os.Args[0])
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+// Log stats to the logger and reset them.
+func logStats() {
+	logger.Printf("Received %d bytes, %d requests\n", bytesReceived, requestsReceived)
+	requestsReceived = 0
+	bytesReceived = 0
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("X-Token") != *token {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(body) == 0 {
+		http.Error(w, "Empty payload", http.StatusBadRequest)
+		return
+	}
+
+	// Update stats
+	bytesReceived += len(body)
+	requestsReceived += 1
+
+	writeLock.Lock()
+	_, err = destConn.Write(body)
+	writeLock.Unlock()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	var err error
+
+	// Load command-line options
+	flag.Parse()
+	flag.Usage = usage
+	if *help {
+		usage()
+	}
+
+	// Setup logger
+	loggerPrefix := fmt.Sprintf("%s[%d] ", os.Args[0], os.Getpid())
+	logger = log.New(os.Stdout, loggerPrefix, log.LstdFlags)
+
+	// Set up UDP connection
+	destConn, err = net.DialTimeout("udp", *out, time.Second)
+	if err != nil {
+		panic(err)
+	}
+
+	// Record stats every minute
+	go func() {
+		statsTicker := time.NewTicker(time.Minute)
+		for {
+			select {
+			case <-statsTicker.C:
+				logStats()
+			}
+		}
+	}()
+
+	// Listen for requests
+	http.HandleFunc("/", handler)
+	err = http.ListenAndServeTLS(*in, *cert, *key, nil)
+	if err != nil {
+		panic(err)
+	}
+}
